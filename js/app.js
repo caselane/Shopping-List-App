299bfeeada1d872bf615e7791131a73546f05e99
Case Rosenfelt <rosenfelt@wisc.edu>
2016-01-29 20:29:22 -0700

    Finishing up

--- js/app.js
+++ js/app.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
 	$('#helpButton').click(function(){
 		$('.help').toggle();
 	});
+	$('#submitItem').click(function(){
+		$('<p class="list-item unchecked"></p>').appendTo('#listArea').html( $(inputItems).val() + '<button class="remove">X</div>')
+	});
+	$("body").on("click", ".list-item", function(){
+		$(this).toggleClass("checked unchecked");
+	});
+	$("body").on("click", ".remove", function(){
+		$(this).closest('p').fadeOut(100);
+	});
+	$('#clearList').click(function(){
+		$(".checked").fadeOut(100);
+	});
 });
