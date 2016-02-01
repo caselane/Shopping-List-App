d963362ab0e36f146949a52c00ddde9eddf5909e
Case Rosenfelt <rosenfelt@wisc.edu>
2016-01-31 20:38:33 -0700

    Finishing touches

--- js/app.js
+++ js/app.js
@@ -3,8 +3,19 @@ $(document).ready(function(){
 		$('.help').toggle();
 	});
 	$('#submitItem').click(function(){
-		$('<p class="list-item unchecked"></p>').appendTo('#listArea').html( $(inputItems).val() + '<button class="remove">X</div>')
+		if(!$.trim($('#inputItems').val())) {
+			alert('You must add a name to the item you are listing before you can list it!');
+		}
+		else {
+			$('<p class="list-item unchecked"></p>').appendTo('#listArea').html( $(inputItems).val() + '<button class="remove">X</div>')
+		}
 	});
+	$('#inputItems').keyup(function(event){
+		if(event.keyCode == 13) {
+			event.preventDefault();
+			$('#submitItem').click();
+		};
+	});	
 	$("body").on("click", ".list-item", function(){
 		$(this).toggleClass("checked unchecked");
 	});
